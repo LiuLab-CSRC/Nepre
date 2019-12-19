99bf79492e90f1523bf831ab3c163b5925ad1f54
hgliu <haiguang.liu@gmail.com>
2019-12-19 10:46:00 +0800

    update run_nepre.sh

--- Cutoff/run_nepre.sh
+++ Cutoff/run_nepre.sh
@@ -4,8 +4,8 @@ pdb_name='native.pdb'
 
 echo 'Calculating energy for a single PDB structure with cutoff=6'
 
-python nepre_f_github.py -s $pdb_folder/$pdb_name $cutoff
+python Nepre_F.py -s $pdb_folder/$pdb_name $cutoff
 
 
 echo 'Calculating energy for PDB structures in a folder with cutoff=6'
-python nepre_f_github.py -m $pdb_folder/ $cutoff
+python Nepre_F.py -m $pdb_folder/ $cutoff
